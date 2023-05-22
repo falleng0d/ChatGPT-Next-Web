607617b8ba76edd8be87529c9d1f53270348e560
Mateus Junior <juniormateusknd@gmail.com>
2023-05-22 18:46:03 -0300

    fix(build.sh): Update build script to create app directory and copy files

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 docker build --tag gptweb .
 docker run -d --name gptweb gptweb
-docker cp gptweb:/app ./build/app
+mkdir -p ./build/app
+docker cp gptweb:/app ./build
 docker stop gptweb
 docker rm gptweb
+scp -r build/app dark-neon.local:/C:/tools/gptweb
